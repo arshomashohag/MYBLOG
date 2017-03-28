a48e2cd8fdf2f2f551c9f38cadc323990573bde9
Md. Shohag Mia <shohag_92_ru@yahoo.com>
2017-03-29 00:13:56 +0600

    Initial Commit

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>                        
+	      </button>
+
+	      <i class="fa fa-comments fa-3x" aria-hidden="true"></i>
+
+
+	    </div>
+	    
+	    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+	      <ul class="nav navbar-nav">
+	        <li class="active"><a href="#">Home</a></li>
+	        <li ><a href="#">Blog</a></li>
+	        <li><a href="#">About</a></li>
+	        <li><a href="#">Contact</a></li>
+	      </ul>
+	       
+	    </div>
+	  </div>
+	</nav>
+
+</header> 
+
+
+
+
+ 
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,42 @@
+ 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>@yield('title')</title>
+	<link rel="stylesheet" type="text/css" href="{{ URL::to('css/main.css') }}">
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	 
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- Font Awesome -->
+	<script src="https://use.fontawesome.com/b3e68927bc.js"></script>
+
+	@yield('styles')
+
+</head>
+
+<body>
+   
+  	 @include('includes.header')
+  
+  
+
+	<div class="main">
+
+		@yield('content')
+		
+	</div>
+
+</body>
+
+</html>
+
+
+
